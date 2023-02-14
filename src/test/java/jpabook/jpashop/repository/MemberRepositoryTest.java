760e71ee869a3ec23704018e01a148f4b363f530
laegel <laegel@selper.co.kr>
2023-02-14 17:56:01 +0900

    프로젝트 초기세팅 및 간단한 테스트

---
+++ src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        assertEquals(savedMember.getId(), findMember.getId());
+        assertEquals(member.getUsername(), findMember.getUsername());
+        assertEquals(member, findMember);
+    }
+
+}
