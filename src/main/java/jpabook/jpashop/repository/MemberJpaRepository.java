1a575a69cc73b4f7d9f965d96d6104ab5c8290d0
laegel <laegel1@gmail.com>
2023-02-14 21:22:39 +0900

    Title : 엔티티 생성 및 테스트

--- src/main/java/jpabook/jpashop/repository/MemberJpaRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.entity.Member;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
@@ -18,5 +21,24 @@ public class MemberJpaRepository {
 
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long memberId) {
+        Member member = em.find(Member.class, memberId);
+        return Optional.ofNullable(member);
+    }
+
+    public Long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
     }
 }
--- src/test/java/jpabook/jpashop/repository/MemberJpaRepositoryTest.java
+++ src/test/java/jpabook/jpashop/repository/MemberJpaRepositoryTest.java
@@ -31,4 +31,5 @@ class MemberJpaRepositoryTest {
 
     }
 
+
 }
