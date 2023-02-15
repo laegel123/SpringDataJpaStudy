5eeba3a82c7f3675854dd65cabf4886d33e64c8e
laegel <laegel@selper.co.kr>
2023-02-15 16:04:51 +0900

    Title : jpa paging test

--- src/test/java/jpabook/jpashop/repository/MemberJpaRepositoryTest.java
+++ src/test/java/jpabook/jpashop/repository/MemberJpaRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -28,6 +30,29 @@ class MemberJpaRepositoryTest {
         assertEquals(findMember.getId(), member.getId());
         assertEquals(findMember.getUsername(), member.getUsername());
         assertEquals(findMember, member);
+
+    }
+
+    @Test
+    public void paging() throws Exception {
+        // given
+        memberJpaRepository.save(new Member("member1", 10));
+        memberJpaRepository.save(new Member("member2", 10));
+        memberJpaRepository.save(new Member("member3", 10));
+        memberJpaRepository.save(new Member("member4", 10));
+        memberJpaRepository.save(new Member("member5", 10));
+
+        int age = 10;
+        int offset = 0;
+        int limit = 3;
+
+        // when
+        List<Member> members = memberJpaRepository.findByPage(age, offset, limit);
+        long totalCount = memberJpaRepository.totalCount(age);
+
+        // then
+        assertEquals(members.size(), 3);
+        assertEquals(totalCount, 5);
 
     }
 
